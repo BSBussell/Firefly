dc668fc4b896b9276c2839a7e8c9f628fc98b01b
Andrew Hillhouse <ahillhou@utk.edu>
2024-04-08 18:07:24 -0400

    added particles effects and spotlight

--- Firefly/Scenes/Checkpoint/checkpoint.gd
+++ Firefly/Scenes/Checkpoint/checkpoint.gd
@@ -1,10 +1,13 @@
 extends Node2D
 
 @onready var checkpoint_sprite = $CheckpointSprite
+@onready var spotlight = $Spotlight
+@onready var explode = $explode
+@onready var lightparticles = $lightparticles
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	spotlight.energy = 0
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -14,3 +17,6 @@ func _process(delta):
 
 func _on_checkpoint_area_area_entered(area):
 	checkpoint_sprite.set_frame(1)
+	spotlight.energy = 1
+	explode.emitting = true
+	lightparticles.emitting = true
