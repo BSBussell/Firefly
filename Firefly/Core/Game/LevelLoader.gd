4e73a605a2763879f35639af56661e94b05e5ace
Bee Bussell <beesbussell@gmail.com>
2024-06-06 15:18:39 -0400

    Fixed Audio, tweaked loading screen.

--- Firefly/Core/Game/LevelLoader.gd
+++ Firefly/Core/Game/LevelLoader.gd
@@ -1,74 +1,74 @@
 extends Node
 class_name LevelLoader
 
-
-@onready var game_view_port = $GameViewPort
-
+## Emitted when the level has been fully cleaned up
 signal level_free
+
+@onready var game_view_port: Viewport = $GameViewPort
 
 var current_level_path: String
 var current_level_instance: Level = null
 
+
+## Returns true if the level has been fully cleaned up
 func finished_cleaning():
 	return game_view_port.get_child_count() == 0
 
-# Called every frame after 
-func _process(delta):
+# Called every frame while cleaning up the level, once the level is cleaned up we emit the level_free signal, and stop processing 
+func _process(delta) -> void:
 	
 	if finished_cleaning():
 		emit_signal("level_free")
 		set_process(false)
 
 
-func begin_threaded_loading(level_path: String):
+func begin_threaded_loading(level_path: String) -> void:
 	ResourceLoader.load_threaded_request(level_path)
 
 
-func load_level(level_path: String, spawn_id: String = ""):
+func load_level(level_path: String, spawn_id: String = "") -> Level:
 	
+	# Update the current level path
 	current_level_path = level_path
 	
 	# Load the PackedScene from the specified path
-	var level_scene = ResourceLoader.load_threaded_get(level_path)
-	# Simulate long load
-	# await get_tree().create_timer(5.0).timeout
+	var level_scene: Resource = ResourceLoader.load_threaded_get(level_path)
+	
 	if level_scene:
 		
 		current_level_instance = level_scene.instantiate()
 		
 		_globals.ACTIVE_LEVEL = current_level_instance
 		
-		print(spawn_id)
 		# If we are given a spawn point id set it
 		if spawn_id:
-			print(spawn_id)
 			current_level_instance.set_spawn_id(spawn_id)
 			
+		# Connect the level loader to the level
 		current_level_instance.connect_level_loader(self)
 		
+		# Add the level to the game_view_port
 		game_view_port.add_child(current_level_instance)
 		
-		
-		
-		
-		
 		return current_level_instance
+	
 	else:
-		print("Failed to load level as a PackedScene at path:", level_path)
+		print("Level failed to be loaded at path:", level_path)
+		return null
 
 func reload_level() -> Level:
 	return await load_level(current_level_path)
 	
 
-func clear_current_level():
+func clear_current_level() -> void:
 	if current_level_instance:
 		current_level_instance.queue_free()
 		current_level_instance = null
 		
-		# Our way of beginning the process of checking when godot frees the level
+		# Begin checking if the level has been cleaned.
 		set_process(true)
 
-func get_current_level():
-	
+## Returns the current level instance
+func get_current_level() -> Level:
 	return current_level_instance
 
--- Firefly/Scripts/Globals/_loader.gd
+++ Firefly/Scripts/Globals/_loader.gd
@@ -3,6 +3,7 @@ extends Node
 var level_loader: LevelLoader
 var ui_loader: UiLoader
 
+## Local path of the currently loaded level
 var current_path: String
 
 signal finished_loading
@@ -10,10 +11,8 @@ signal finished_loading
 ## If the game is currently loading a level
 var loading: bool = true
 
-#@onready var loading_screen = preload("res://Core/loading_screen.tscn").instantiate()
-
-
-func show_loading():
+## Loads and displays the loading screen
+func show_loading() -> Control:
 	var loading_screen = preload("res://Core/loading_screen.tscn").instantiate()
 	if ui_loader:
 		ui_loader.add_child(loading_screen)
@@ -23,20 +22,23 @@ func show_loading():
 		await loading_screen.animation_player.animation_finished
 	return loading_screen  # Return the instance to manage it later
 
-func hide_loading(loading_screen):
+## Hides and unloads the loading screen
+func hide_loading(loading_screen) -> void:
 	loading_screen.play_animation("load_out")
 	await loading_screen.animation_player.animation_finished
 
+## Connects the level loader and ui loader to the global loader so it can synchronize them
 func connect_loaders(ll: LevelLoader, ui: UiLoader):
 	level_loader = ll
 	ui_loader = ui
 
 	
-
+## Loads a level using a given path
 func load_level(path: String, spawn_id: String = ""):
 
 	loading = true
 
+	# Save the current level path
 	current_path = path
 
 	# Begin loading the level in the background
@@ -56,7 +58,6 @@ func load_level(path: String, spawn_id: String = ""):
 	# Free the level
 	level_loader.clear_current_level()
 
-	
 	# Free the Ui
 	ui_loader.reset_ui()
 	
@@ -70,29 +71,28 @@ func load_level(path: String, spawn_id: String = ""):
 	# Load new ui
 	ui_loader.setup(level)
 
-	# Unpause 
+	# Unpause the game if it was paused for some reason
 	get_tree().paused = false
 
 	# Restoring the players level
 	if _globals.ACTIVE_PLAYER:
 		_globals.ACTIVE_PLAYER.glow_manager.change_state(player_level)
 
+	# Set loading to false
 	loading = false
 
+	# Emit signal to notify that the level has finished loading to any listeners
 	emit_signal("finished_loading")
 
+	# Hide the loading screen
 	await hide_loading(loading_screen)
 
-	
-
-	
-
-	# Once we've finished loading start the timer
+	# Once the screen is uncovered, resume the timer
 	_stats.start_timer()
 
 
-func reload_level():
-	load_level(current_path)
+func reload_level(spawn_id: String = ""):
+	load_level(current_path, spawn_id)
 
 func reset_game(level_path: String):
 
