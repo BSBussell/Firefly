312b8c88514f6426bb2bcd5fec19b8d6cd0dd714
Bee Bussell <beesbussell@gmail.com>
2024-09-17 16:05:57 -0400

    Improved Gliding Control
    Simple Common Sense Pass

--- Firefly/Scripts/Player/PlayerStates/Gliding.gd
+++ Firefly/Scripts/Player/PlayerStates/Gliding.gd
@@ -11,20 +11,39 @@ class_name Gliding
 @export_subgroup("Temp Knobs")
 @export var glide_y_boost: float = -200
 @export var glide_x_boost: float = 80
+
+## The speed multiplier when reversing directions
 @export var reverse_boost: float = -0.9
 
-@export var accel: float = 90
-
+## How strong gravity is when floating
 @export var grav_speed: float = 100
+
+## How quickly we approach the gravity speed
 @export var grav_accel: float = 500
 
-@export var air_resistance: float = 100
+## The multiplier applied to friction. Applied when we stop pressing a button.
+@export var air_frict_multiplier: float = 0.6
 
+## The multipler applied to the speed reduction when we're over the speed limit.
+@export var glide_reduction: float = 0.8
+
+## Speed multiplier when gliding, as you glide your speed approaches your air speed times this.
+@export var glide_speed_floor_multi: float = 0.9
+
+## How quickly your glide speed decreases
+@export var glide_speed_reduct: float = 10
 
 @onready var flap_sfx = $"../../Audio/FlapSFX"
 
 
+## This is the max accel we can reach while gliding.
 var glide_speed: float = 0.0
+
+# When we start gliding, the glide speed is set to a speed floor that slowly decreases.
+# Also floors the speed when direction is changed/speed equals 0.
+# This decision was made to de-incentize just holding glide everywhere.
+# I didn't want gliding constantly to be the fastest movement option.
+
 
 # Called on state entrance, setup
 func enter() -> void:
@@ -127,7 +146,7 @@ func process_physics(delta: float) -> PlayerState:
 	apply_gravity(delta)
 	
 	apply_acceleration(delta, parent.horizontal_axis)
-	apply_air_resistance(delta)
+	apply_air_resistance(delta, parent.horizontal_axis)
 	
 	return state_status()
 	
@@ -136,7 +155,48 @@ func process_physics(delta: float) -> PlayerState:
 func animation_end() -> PlayerState:
 	return null
 
+	
+func apply_gravity(delta: float) -> void:
+	
+	# TODO: Strengthen gravity as x_velocity decreases
+	var max_fall_speed: float = grav_speed
+	parent.velocity.y = move_toward(parent.velocity.y, max_fall_speed, grav_accel * delta)
 
+## Addes Acceleration when the player holds a direction, the longer the player is in the air the slower they should move
+func apply_acceleration(delta, direction) -> void:
+
+	# If we're moving in a direction
+	if direction:
+
+		var airAccel: float
+
+		# Slowing ourselves down in the air
+		if (abs(parent.velocity.x) > parent.air_speed and sign(parent.velocity.x) == sign(direction)):
+			airAccel = parent.movement_data.AIR_SPEED_RECUTION * glide_reduction
+
+		# Speed ourselves upd  009
+		else:
+			airAccel = parent.air_accel
+
+		parent.velocity.x  = move_toward(parent.velocity.x, glide_speed*direction, airAccel * delta)
+
+
+
+# Over time slow the players x velocity to 0 
+func apply_air_resistance(delta: float, direction: float) -> void:
+	
+	if not direction:
+		
+		var glide_stopping: float = parent.air_frict * air_frict_multiplier
+		
+		parent.velocity.x = move_toward(parent.velocity.x, 0.0, delta * glide_stopping)
+		
+		# If velocity reaches 0 while we arent holding a direction, then we floor the max glide_speed
+		if parent.velocity.x == 0:
+			glide_speed = parent.air_speed * glide_speed_floor_multi
+	
+	glide_speed = move_toward(glide_speed, parent.air_speed * glide_speed_floor_multi, delta * glide_speed_reduct)
+	
 ## Returns what state we should exit to. Contains 
 func state_status() -> PlayerState:
 	
@@ -171,35 +231,3 @@ func state_status() -> PlayerState:
 	
 	
 	return null
-	
-func apply_gravity(delta: float) -> void:
-	
-	# TODO: Strengthen gravity as x_velocity decreases
-	var max_fall_speed: float = grav_speed
-	parent.velocity.y = move_toward(parent.velocity.y, max_fall_speed, grav_accel * delta)
-
-## Addes Acceleration when the player holds a direction, the longer the player is in the air the slower they should move
-func apply_acceleration(delta, direction) -> void:
-
-	# If we're moving in a direction
-	if direction:
-
-		var airAccel: float
-
-		# Slowing ourselves down in the air
-		if (abs(parent.velocity.x) > parent.air_speed and sign(parent.velocity.x) == sign(direction)):
-			airAccel = parent.movement_data.AIR_SPEED_RECUTION * 0.8
-
-		# Speed ourselves upd  009
-		else:
-			airAccel = parent.air_accel
-
-		parent.velocity.x  = move_toward(parent.velocity.x, glide_speed*direction, airAccel * delta)
-
-# Over time slow the players x velocity to 0 
-func apply_air_resistance(delta: float) -> void:
-	
-	glide_speed = move_toward(glide_speed, parent.air_speed * 0.9  , delta * 10)
-	
-	#if parent.velocity.x != 0:
-		#parent.velocity.x = move_toward(parent.velocity.x, 0, air_resistance * delta)
