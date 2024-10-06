7abd924f020b4b24ac093255ed65397f83430bfd
Bee Bussell <beesbussell@gmail.com>
2024-10-06 01:12:06 -0400

    Fix Initial Loading Screen Scale

--- Firefly/Core/loading_screen.gd
+++ Firefly/Core/loading_screen.gd
@@ -5,15 +5,7 @@ extends ColorRect
 
 func _ready():
 
-	# Calculate screen size
-	var render_size = DisplayServer.window_get_size()
-
-	# Divide Screen size by 15
-	var triangle_size: int = render_size.x 
-
-
-	# Get the shader material attached to the ColorRect
-	material.set("shader_parameter/diamondPixelSize", triangle_size)
+	scale_shader()	
 
 	progress_bar.value = 0
 
@@ -30,6 +22,8 @@ func _process(_delta):
 
 	# Set the progress bar value
 	progress_bar.set_target_value(progress[0] * 100.0)
+	
+	scale_shader()
 
 
 func play_animation(animation: String):
@@ -40,3 +34,15 @@ func _on_animation_player_animation_finished(anim_name:StringName):
 	
 	if anim_name == "load_in":
 		play_animation("bounce")
+		
+func scale_shader():
+	# Calculate screen size
+	var render_size = DisplayServer.window_get_size()
+
+	# Divide Screen size by 15
+	var triangle_size: int = render_size.x 
+
+
+	# Get the shader material attached to the ColorRect
+	material.set("shader_parameter/diamondPixelSize", triangle_size)
+	
