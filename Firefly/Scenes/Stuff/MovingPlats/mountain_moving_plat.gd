1ab5f2552998709cfa8d490c00372510d5e6f8b1
Bee Bussell <beesbussell@gmail.com>
2024-11-10 16:22:22 -0500

    Fixed Moving Plat Player Launching

--- Firefly/Scenes/Stuff/MovingPlats/mountain_moving_plat.gd
+++ Firefly/Scenes/Stuff/MovingPlats/mountain_moving_plat.gd
@@ -1,5 +1,6 @@
 extends MovingPlat
 
+@onready var mtn_plat_3x = $mtn_plat_3x
 
 func _ready():
 	deactivate()
@@ -11,3 +12,11 @@ func _on_mtn_plat_3x_player_landed():
 # Moving Plat events
 func _on_cycle_finished():
 	deactivate()
+
+
+func _on_mtn_plat_3x_player_left():
+	
+
+	if _globals.ACTIVE_PLAYER.velocity.y < _globals.ACTIVE_PLAYER.jump_velocity:
+		print(_globals.ACTIVE_PLAYER.velocity.y)
+		_globals.ACTIVE_PLAYER.launched = true
