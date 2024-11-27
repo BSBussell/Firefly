1012889a1245305e8221a9a3286c39b0eb63d46b
Bee Bussell <beesbussell@gmail.com>
2024-11-27 15:09:30 -0500

    Polish Collectible "Checklist"

--- Firefly/Scenes/UI_Elements/CollectibleCounters/TotalCollectibleCounter.gd
+++ Firefly/Scenes/UI_Elements/CollectibleCounters/TotalCollectibleCounter.gd
@@ -6,6 +6,10 @@ class_name TotalJarCounter
 @export var GAME_COUNTER: Label
 @export var COUNTER_ANIMATOR: AnimationPlayer
 @export var ANIMATION_TIMER: Timer
+@export var LEVEL_GLITTER: CPUParticles2D
+@export var TOTAL_GLITTER: CPUParticles2D
+
+@export var shine_color: Color
 
 var out: bool = false
 
@@ -28,32 +32,90 @@ func _ready():
 	
 	
 	
+func process_jars(jars: Array):
+	var yellow_collected = 0
+	var blue_collected = 0
+	var yellow_total = 0
+	var blue_total = 0
 
+	for jar in jars:
+		if jar["blue"]:
+			blue_total += 1
+			if jar["nabbed"]:
+				blue_collected += 1
+		else:
+			yellow_total += 1
+			if jar["nabbed"]:
+				yellow_collected += 1
+		
+	return [yellow_total, yellow_collected, blue_total, blue_collected]
 
+	
+func update_labels() -> void:
+	
+	var level_jars: Array = _jar_tracker.get_level_jars(_globals.ACTIVE_LEVEL.id)
+	var processed_totals: Array = process_jars(level_jars)
+	
+	
+	var yellow_collected: int = processed_totals[1]
+	var yellow_max: int = processed_totals[0]
+	var blue_collected: int = processed_totals[3]
+	var blue_max: int = processed_totals[2]
+
+	var total_collected: int = processed_totals[1] + processed_totals[3]
+	
+	## Effects
+	
+	# If the number of collected yellow jars
+	if yellow_collected >= yellow_max:
+		LEVEL_COUNTER.add_theme_color_override("font_color", shine_color)
+	else:
+		LEVEL_COUNTER.remove_theme_color_override("font_color")
+		
+	if blue_collected >= blue_max:
+		LEVEL_GLITTER.emitting = true
+		LEVEL_GLITTER.resolution_update()
+	else:
+		LEVEL_GLITTER.emitting = false
+	
+	LEVEL_COUNTER.text = "%d/%d" % [total_collected, yellow_max]
+	
+	
+	# Game - Wide label
+	var all_jars: Array = _jar_tracker.known_jars.values()
+	processed_totals = process_jars(all_jars)
+	
+	
+	yellow_collected = processed_totals[1]
+	yellow_max = processed_totals[0]
+	blue_collected = processed_totals[3]
+	blue_max = processed_totals[2]
+
+	total_collected = processed_totals[1] + processed_totals[3]
+	
+	## Effects
+	
+	# If we got all the yellows turn yellow
+	if yellow_collected >= yellow_max:
+		GAME_COUNTER.add_theme_color_override("font_color", shine_color)
+	else:
+		GAME_COUNTER.remove_theme_color_override("font_color")
+		
+	# If we got all the blues sparkle
+	if blue_collected >= blue_max:
+		TOTAL_GLITTER.emitting = true
+		TOTAL_GLITTER.resolution_update()
+	else:
+		TOTAL_GLITTER.emitting = false
+	
+	# Display Text
+	GAME_COUNTER.text = "%d/%d" % [total_collected, yellow_max]
+	
 	
 func show_counter():
 	
-	# Get the levels jars thar are not blue
-	var yellow_jars: Array = _jar_tracker.filter(func (value): 
-		
-		var level_match: bool = value["level_id"] == _globals.ACTIVE_LEVEL.id
-		var is_yellow: bool = value["blue"] == false
-		return level_match and is_yellow
-		
-	)
-	var jar_max: int = yellow_jars.size()
 	
-	
-	var collected: int = _jar_tracker.num_found_jars(_globals.ACTIVE_LEVEL.id)
-	
-	LEVEL_COUNTER.text = "%d/%d" % [collected, jar_max]
-	
-	collected = _jar_tracker.total_num_found_jars()
-	jar_max = _jar_tracker.filter(func (value):
-		return value["blue"] == false
-	).size()
-	
-	GAME_COUNTER.text  = "%d/%d" % [collected, jar_max]
+	update_labels()
 	
 	
 	if not out:
